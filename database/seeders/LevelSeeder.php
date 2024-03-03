fe17ccaec167692fd020a311e703341a9f2fa9eb
nurul mustofa <nioke8090@gmail.com>
2024-03-03 22:09:06 +0700

    Trial Eloquent ORM Success

--- database/seeders/LevelSeeder.php
+++ database/seeders/LevelSeeder.php
@@ -18,6 +18,7 @@ class LevelSeeder extends Seeder
             ['level_id' => 1, 'level_kode' => 'ADM', 'level_nama' => 'Administrator', 'created_at' => now(), 'updated_at' => now()],
             ['level_id' => 2, 'level_kode' => 'MNG', 'level_nama' => 'Manager', 'created_at' => now(), 'updated_at' => now()],
             ['level_id' => 3, 'level_kode' => 'STF', 'level_nama' => 'Staff/Kasir', 'created_at' => now(), 'updated_at' => now()],
+            ['level_id' => 4, 'level_kode' => 'CUS', 'level_nama' => 'Customer', 'created_at' => now(), 'updated_at' => now()],
         ];
         DB::table('m_level')->insert($data);
     }
