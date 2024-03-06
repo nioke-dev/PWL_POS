3c9d9d7b1ef6845a98cd8681c601070c50f1b208
nurul mustofa <nioke8090@gmail.com>
2024-03-06 17:56:56 +0700

    js4 praktikum 2.1 retrieving single model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,16 +25,18 @@ class UserController extends Controller
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
         // coba akses model userModel
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
