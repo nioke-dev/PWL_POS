583aa09bb3dec2c7ab1c66b79fd4e0ce8c4a9f77
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2024-04-03 22:46:47 +0700

    js 7 praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome2');
+    return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
